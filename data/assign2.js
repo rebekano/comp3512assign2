5c9000826d922a66e64e24f0377bc51c7d3d116a
hnijj156 <hnijj156@mtroyal.ca>
2023-12-08 23:11:19 -0700

    creating playlist page and radar chart

--- data/assign2.js
+++ data/assign2.js
@@ -40,7 +40,7 @@ document.addEventListener("DOMContentLoaded", function () {
 
 
 
-    //  songApi.forEach(song => {
+    //  songsApi.forEach(song => {
     //     listOutput(song.title, document.querySelector("#titleSearch")); 
     //  })
     //  const titlesort = song.sort((a,b) => a.title.localeCompare(b.title)); 
@@ -68,13 +68,13 @@ document.addEventListener("DOMContentLoaded", function () {
     songsApi.sort((a, b) => a.title.toLowerCase() > b.title.toLowerCase() ? 1 : -1);    
     generateTable(songsApi);
 
-    artist.forEach((artist) => {
-        outputList(artist.name, document.querySelector("#artistSearch"));
-    })
+    // artist.forEach((artist) => {
+    //     outputList(artist.name, document.querySelector("#artistSearch"));
+    // })
 
-    genre.forEach((genre) => {
-        outputList(genre.name, document.querySelector("genreSearch"));
-    })
+    // genre.forEach((genre) => {
+    //     outputList(genre.name, document.querySelector("genreSearch"));
+    // })
 
 
         document.querySelector('#table').addEventListener('click', function (e) {
@@ -86,19 +86,19 @@ document.addEventListener("DOMContentLoaded", function () {
         
             switch (selectId) {
                 case 'Title':
-                    sortTable(samp, 'title', 'title');
+                    sortTable(songsApi, 'title', 'title');
                     break;
                 case 'Artist':
-                    sortTable(samp, 'artist.name', 'artist');
+                    sortTable(songsApi, 'artist.name', 'artist');
                     break;
                 case 'Year':
-                    sortTable(samp, 'year', 'year');
+                    sortTable(songsApi, 'year', 'year');
                     break;
                 case 'Genre':
-                    sortTable(samp, 'genre.name', 'genre');
+                    sortTable(songsApi, 'genre.name', 'genre');
                     break;
                 case 'Popularity':
-                    sortTable(samp, 'details.popularity', 'popularity');
+                    sortTable(songsApi, 'details.popularity', 'popularity');
                     break;
                 default:
                     break;
@@ -204,29 +204,200 @@ document.addEventListener("DOMContentLoaded", function () {
         // creating and adding songs to playlist 
         function addPlaylist() {
             const buttons = document.querySelectorAll('.addButton');
+
+            for(let b of button){
+                b.addEventListener('click', function(){
+                    const found = songsApi.find(s.song_id == b.getAttribute('id')); 
+                    if(!added,includes(found)) {
+                        added.push(found); 
+                        console.log('song has been added');
+                    }
+                    else {
+                        console.log('song has already been added'); 
+                    }
+                })
+            }
          
-            buttons.forEach(button => {
-                button.addEventListener('click', function () {
-                    const songId = button.getAttribute('id');
-                    const found = songsApi.find(song => song.song_id == songId);
+            // buttons.forEach(button => {
+            //     button.addEventListener('click', function () {
+            //         const songId = button.getAttribute('id');
+            //         const found = songsApi.find(song => song.song_id == songId);
          
-                    if (found && !added.includes(found)) {
-                        added.push(found);
-                        console.log('Song Has Been Added');
-                    } else {
-                        console.log('Song Has Already Been Added');
-                        console.log('Duplicate');
-                    } // Change these to display as a pop-up***
-                });
-            });
+            //         if (found && !added.includes(found)) {
+            //             added.push(found);
+            //             console.log('Song Has Been Added');
+            //         } else {
+            //             console.log('Song Has Already Been Added');
+            //             console.log('Duplicate');
+            //         } // Change these to display as a pop-up***
+            //     });
+            // });
          }
+
 
          // function to display the playlist view 
 
+         function playtable(songspassed, table){
+           table.innerHTML = "";
+            const headers = ['Title', 'Artist', 'Year', 'Genre', 'Popularity', 'Playlist'];
+
+            const headerRow = document.createElement('tr'); 
+
+            headers.forEach(headerText => {
+                const header = document.createElement('th'); 
+                header.setAttribute('id',headerText);
+                const innerHTML = document.createTextNode(headerText); 
+                header.appendChild(textNode); 
+                headerRow.appendChild(header); 
+            }); 
+
+            table.appendChild(headerRow); 
+
+            for(let sp of songspassed) {
+                const tr = document.createElement('tr');
+                const title = document.createElement('td');
+
+                title.textContent = sp.artist.name; 
+                tr,appendChild(artist);
+
+                const year = document.createElement('td'); 
+                year.innerHTML = sp.year;
+                tr.appendChild(year);
+
+                const genre = document.createElement('td');
+                genre.innerHTML=sp.genre.name;
+                tr.appendChild(genre);
+
+                const popularity = document.createElement('td');
+                popularity.innerHTML = s.details.popularity;
+                tr.appendChild(popularity);
+
+                const button = document.createElement('button');
+                button.setAttribute('id', sp.song_id); 
+                button.setAttribute('class', 'removebutton'); 
+                button.textContent = "Remove";
+                tr.appendChild(button); 
+
+                table.appendChild(tr); 
+                
+
+                [title,artist,yrar,genre,popularity].forEach((title) => {
+                    title.addEventListener("click", function(){
+                        songinformation = document.querySelector('#songView');
+                        display = document.querySelector('#playlistView');
+                        songinformation.classList.toggle('hidden');
+                        display.classList.toggle('hidden'); 
+
+                        const returnback = document.createElement("button");
+                        const viewpagetitle = document.createElement("h1"); 
+                        const songtitle = document.createElement("h3");
+                        const songartist = document.createElement("h3");
+                        const songgenre = document.createElement("h3");
+                        const songyear = document.createElement("h3");
+                        const songduration = document.createElement("h3"); 
+
+                        returnback.textContent = "Back to playlist";
+                        returnback.setAttribute("class", "BackButton"); 
+                        songinformation.appendChild(goback);
+                        goback.addEventListener("click", function() {
+                            songinformation.classList.toggle('hidden');
+                            display.classList.toggle('hidden');
+                            songinformation.innerHTML = ""; 
+
+                        
+
+                        });
+
+                        const bpm = document.createElement("li"); 
+                        bpm.textContent = "BPM: " +sp.details.bpm;
+                        songdetails.appendChild(bpm);
+                        const energy = document.createElement("li"); 
+                        energy.textContent = "Energy: " +sp.analytics.energy;
+                        songdetails.appendChild(energy);
+                        const dance = document.createElement("li"); 
+                        dance.textContent = "Danceability: " +sp.analytics.danceability;
+                        songdetails.appendChild(dance);
+                        const liveness = document.createElement("li"); 
+                        liveness.textContent = "Liveness: " +sp.analytics.liveness;
+                        songdetails.appendChild(liveness);
+                        const valence = document.createElement("li"); 
+                        valence.textContent = "Valence: " +sp.analytics.valence;
+                        songdetails.appendChild(valence);
+                        const acoustic = document.createElement("li"); 
+                        acoustic.textContent = "Acousticness: " +sp.analytics.acoustic;
+                        songdetails.appendChild(acoustic);
+                        const speech = document.createElement("li"); 
+                        speech.textContent = "Speechiness: " +sp.analytics.speechiness;
+                        songdetails.appendChild(speech);
+                        const popularity = document.createElement("li"); 
+                        popularity.textContent = "Popularity: " +sp.details.popularity;
+                        songdetails.appendChild(popularity);
+
+
+                        const informationdiv = document.createAttribute("div");
+                        informationdiv.setAttribute("class", "song information");
+                        const songdetails = document.createElement("div");
+                        songdetails.setAttribute("class", "songDetails");
+
+                        viewpagetitle.textContent = "song information"; 
+                        informationdiv.appendChild(viewpagetitle);
+
+                        songtitle.textContent = "Title: " + sp.title;
+                        informationdiv.appendChild(songtitle); 
+
+                        songartist.textContent = "Artist: " + sp.artist.name;
+                        informationdiv.appendChild(songartist); 
+
+                        songgenre.textContent = "Genre: " + sp.genre.name;
+                        informationdiv.appendChild(songgenre); 
+
+                        songyear.textContent = "Year: " + sp.year;
+                        informationdiv.appendChild(songyear); 
+
+                        songduration.textContent = "Duration: " + timeduration(sp.details.duration); 
+                        informationdiv.appendChild(songduration); 
+
+                           
+                        songinformation.appendChild(informationdiv);
+                        informationdiv.appendChild(songdetails); 
+
+                        songinformation.appendChild(makedata(s));
+
+                    });
+                });
+
+             }
+
+             playtable(); 
+
+             // Number of the songs in the list 
+             let songsinlist = document.querySelector('#playlistInfo'); 
+
+             songsinlist.innerHTML = `Songs in the List for the Playlist: ${added.length}`;
+         }
+    
+
+         // view playlist button 
+         document.querySelector("#playlistView").addEventListener("click", function(e){
+            e.preventDefault(); 
+
+            const playView = document.querySelector("#playlistView"); 
+            const maketable = document.createElement('table');
+
+            playlistView.appendChild(maketable); 
+            const playtable = document.querySelector("#playlistView"); 
+            const playlistView = docuemnt.querySelector("#playlistView"); 
+            const searchPage = document.quertselector("#searchView"); 
+            playlistView.classList.toggle('hidden'); 
+            searchPage.classList.toggle('hidden'); 
+
+            playtable(added, playtable); 
+         });
+         
 
          // function to remove playlist 
          function removeplaylist(){
-            let generateTable = document.querySelector('#playlistview'); 
+            const generateTable = document.querySelector('#playlistview'); 
             const button = document.querySelectorAll('.removebtn'); 
 
             for(let b of button){
@@ -266,6 +437,58 @@ document.addEventListener("DOMContentLoaded", function () {
 
 
          // radar analytics 
+         function makedata(song){
+            const div = document.createElement("div");
+            let radardiv = document.createElement("div");
+            radardiv.setAttribute('id', 'radarContainer');
+            let canvas = document.createElement("canvas");
+            canvas.setAttribute('id', 'radarchart'); 
+
+            radardiv.appendChild(canvas);
+            radardiv.style.width = 500 + "px";
+            radardiv.style.height = '500px';
+
+            chart(canvas, song);
+            div.appendChild(radardiv);
+            return div;
+         }
+
+         function chart(canvas, song){
+            console.log(song)
+            new chart(canvas, {
+                type: 'Radar chart',
+                data: {
+                    labels: [
+                    'Danceability',
+                    'energy',
+                    'valence',
+                    'speechiness',
+                    'loudness',
+                    'Liveness'
+                ], 
+                datasets: [{
+                    label: 'Song Analytics',
+                    data: [
+                        song.analytics.danceability,
+                        song.analytics.energy,
+                        song.analytics.valence,
+                        song.analytics.speechiness,
+                        song.analytics.acoustic,
+                        song.analytics.liveness
+                    ],
+                    Fill: true,
+                    backgroundColor: 'rgba(255, 99, 132, 0.2)',
+                    borderColor: 'rgb(255, 99, 132)',
+                    pointBackgroundColor: 'rgb(255, 99, 132)',
+                    pointBorderColor: '#fff',
+                    pointHoverBackgroundColor: '#fff',
+                    pointHoverBorderColor: 'rgb(255, 99, 132)'
+                }]
+                }, 
+
+                
+            })
+         }
 
     });
   
